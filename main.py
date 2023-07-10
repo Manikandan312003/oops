5ede3ec000b4a5a60126337cc84569e10b405d30
Viswa R <viswarv11@gmail.com>
2023-07-11 05:11:41 +0530

    call method

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-class java():
-    def __init__(self):
+class laptop():
+    def print(self):
+        print("Laptop")
+
+class Desktop():
+    def print(self):
+        print("Desktop")
+
+class dev():
+    def print(self,java):
+        try:
+            (java.print())
+        except:
+            print("The object you passed not belongs to the laptop or Desktop object")
+lap=laptop()
+des=Desktop()
+d=dev()
+d.print(lap)
+d.print(des)
